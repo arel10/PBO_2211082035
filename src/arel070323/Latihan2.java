8ea85e2da9932cf43e081ab5d5d769c0f07c7535
arel10 <afchaarelpratama@gmail.com>
2023-03-08 11:28:48 +0700

    Initial commit

---
+++ src/arel070323/Latihan2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arel070323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Latihan2 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 45;
+        float ratarata;
+        
+        ratarata = (a+b+c)/3;
+        System.out.println("number1 = "+a);
+        System.out.println("number2 = "+b);
+        System.out.println("number3 = "+c);
+        System.out.println("Avarage is = "+ratarata);
+        
+        
+    }
+    
+}
